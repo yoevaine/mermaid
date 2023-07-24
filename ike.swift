0860ba96dc466126100624e827418893399c7c62
yoevaine <140435783+yoevaine@users.noreply.github.com>
2023-07-24 19:20:07 +0100

    Create ike.swift

---
+++ ike.swift
@@ -0,0 +1,60 @@
+graph TB
+
+A["1. About Yourself"] --> B["Education: Master's degree in Physician Assistant Studies"]
+B --> BA["University: Name of University"]
+B --> BB["Year of Graduation: Year"]
+A --> C["Experience: Research on mental health literacy and help-seeking behaviors"]
+C --> CA["Specific Projects: Details about the research projects"]
+C --> CB["Findings: Key findings from the research"]
+A --> D["Current Role: Support Worker at Standby Healthcare in Newcastle"]
+D --> DA["Responsibilities: Details about the responsibilities in current role"]
+D --> DB["Achievements: Key achievements in current role"]
+
+E["2. Career in Psychiatry"] --> F["Interest: Details about how the interest in psychiatry developed"]
+F --> FA["Influences: Key influences that led to the interest in psychiatry"]
+E --> G["Dissertation: Details about the dissertation on mental health literacy and help-seeking behaviors"]
+G --> GA["Key Findings: Key findings from the dissertation"]
+G --> GB["Implications: Implications of the research for the field of psychiatry"]
+
+H["3. Applying for Position at Trust"] --> I["Interest: Reasons for applying for the position"]
+H --> J["Alignment: How the Trust's commitment to quality psychiatric care aligns with personal interests and skills"]
+
+K["4. Knowledge about Trust"] --> L["Services: Understanding about the mental health, learning disability, and eating disorders services provided by the Trust"]
+L --> LA["Specific Services: Details about specific services that the Trust provides"]
+K --> M["Focus: Understanding about the Trust's focus on providing the highest quality of care and improving lives"]
+M --> MA["Specific Initiatives: Details about specific initiatives undertaken by the Trust to improve care"]
+
+N["5. Contribution to Trust"] --> O["Background: How a background in medicine and public health would contribute to the Trust"]
+N --> P["Skills: Specific skills that would be beneficial to the Trust"]
+N --> Q["Experience: Examples of leading teams and managing projects that demonstrate ability to contribute to the Trust"]
+
+R["6. Keeping Up-to-date"] --> S["Membership: Details about membership in the Royal Society of Public Health"]
+R --> T["Activities: Specific activities like reading medical journals, attending webinars and conferences, and participating in training and courses that help stay up-to-date"]
+
+U["7. Leadership Example"] --> V["Example: Detailed example about coordinating team's efforts during a presentation at University of Sunderland"]
+
+W["8. Dealing with Difficult Patient/Family"] --> X["Example: Detailed example about managing a challenging situation with patience and empathy at Nurseplus"]
+
+Y["9. Difficult Decision"] --> Z["Example: Detailed example about making a difficult decision to suggest a change in the course of treatment at Joedan Hospital in Nigeria"]
+
+AA["10. Continuous Learning and Professional Development"] --> AB["Education: Details about pursuing MSc in Public Health and other additional qualifications"]
+AA --> AC["Professional Development: Details about efforts to enhance Project Management skills and other professional development activities"]
+
+AD["11. Working as Part of a Multidisciplinary Team"] --> AE["Example: Detailed example about participating in patient consultations, treatment planning, and follow-up care during an internship at Medical University Pleven"]
+
+AF["12. Handling Aggressive Patient"] --> AG["Example: Detailed example about using de-escalation techniques during a consultation in a psychiatry department"]
+
+AH["13. Patient Refusing Medication"] --> AI["Example: Detailed example about engaging in a conversation with the patient to explain the importance of the medication"]
+
+AJ["14. Disagreement with Treatment Plan"] --> AK["Example: Detailed example about taking the time to explain the reasons behind the treatment plan and address the family's concerns"]
+
+AL["15. Suspected Inappropriate Behavior"] --> AM["Example: Detailed example about how to handle suspected inappropriate behavior"]
+
+AN["16. Prioritizing Tasks"] --> AO["Example: Detailed example about how to prioritize tasks based on urgency and importance"]
+
+AP["17. Interest in Trust"] --> AQ["Reasons: Detailed reasons for being drawn to the Trust's commitment to providing high-quality mental health, learning disability, and eating disorder services"]
+
+AR["18. Professional Growth at Trust"] --> AS["Opportunities: Details about the opportunities to work in a multidisciplinary team dedicated to mental health that would aid professional growth"]
+
+AT["19. Contributing to Trust's Mission and Values"] --> AU["Commitment: Details about personal commitment to improving the quality of care and working together for patients and how it aligns with the Trust's mission and values"]
+
